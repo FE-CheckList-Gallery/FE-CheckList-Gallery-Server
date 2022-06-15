40b96b7e0a6102050e02dbe4ebacca85187ed25f
Juhyeon <gomi.dev1755@gmail.com>
2022-06-15 22:57:46 +0900

    Feat: morgan(log library)세팅

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import path from 'path'
 import { v1Router } from './routers'
 import { errorHandler } from './middlewares'
 import passportInit from './middlewares/passport'
+import morgan from 'morgan'
 
 const app = express()
 passportInit()
 
 // CORS 에러 방지
 app.use(cors())
+
+// request console
+app.use(morgan('tiny'))
 
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json())
@@ -19,7 +23,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 // v1 router.
 app.use('/', v1Router)
-
+app.get('/', (req, res) => {
+  res.send('FE-CheckList-Gallery-API')
+})
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
 app.use(errorHandler)
