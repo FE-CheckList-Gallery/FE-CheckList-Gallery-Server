f0af85386e804e408e64917ad7878b9c1cea62c2
Juhyeon <gomi.dev1755@gmail.com>
2022-06-21 04:34:29 +0900

    Fix: auth redirect

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -9,7 +9,12 @@ authRouter.get('/github', passport.authenticate('github', { scope: ['user:email'
 authRouter.get('/github/callback', passport.authenticate('github', { failureRedirect: '/auth/github', session: false }),
   function (req, res) {
     const token = jwt.sign(req.user, process.env.JWT_SECRET_KEY, { expiresIn: process.env.ACCESSTOKEN_EXPIRE })
-    res.status(200).json({ token, user: req.user })
+    res
+      .header({
+        token,
+        user: req.user
+      })
+      .redirect(process.env.FRONTEND_HOME_URL)
   })
 
 export default authRouter
