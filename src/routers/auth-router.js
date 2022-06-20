5837132d7579d6902ca91669ff9f6dee409e56d5
Juhyeon <gomi.dev1755@gmail.com>
2022-06-21 05:37:52 +0900

    Fix: cookie 만료시간

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -11,11 +11,11 @@ authRouter.get('/github/callback', passport.authenticate('github', { failureRedi
     const token = jwt.sign(req.user, process.env.JWT_SECRET_KEY, { expiresIn: process.env.ACCESSTOKEN_EXPIRE })
     res
       .cookie('token', token, {
-        maxAge: 60 * 60 * 1000 * 24,
+        maxAge: 60 * 60 * 1000 * 30,
         domain: process.env.FRONTEND_HOME_URL
       })
       .cookie('user', req.user, {
-        maxAge: 60 * 60 * 1000 * 24,
+        maxAge: 60 * 60 * 1000 * 30,
         domain: process.env.FRONTEND_HOME_URL
       })
       .redirect(process.env.FRONTEND_HOME_URL)
