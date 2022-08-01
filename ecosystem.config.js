4a3ec7004c74ede732b7eae786ad4c40ec13b307
Juhyeon <gomi.dev1755@gmail.com>
2022-08-01 23:31:30 +0900

    fix: deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'fcgserver',
-      script: 'npm run server',
+      name: "fe-checklist-gallery",
+      script: './server-register.js',
       watch_delay: 5000,
-      ignore_watch: ['node_modules', 'src/uploads']
-    }
-  ]
-
-}
+      ignore_watch: ["node_modules", "src/uploads"],
+    },
+  ],
+};
