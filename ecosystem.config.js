ccb85574fe576c60f55c3f1328854c8d38e88d50
Juhyeon <gomi.dev1755@gmail.com>
2022-08-02 00:22:34 +0900

    fix: add engines info

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "fe-checklist-gallery",
+      name: 'fe-checklist-gallery',
       script: './server-register.js',
       watch_delay: 5000,
-      ignore_watch: ["node_modules", "src/uploads"],
-    },
-  ],
-};
+      ignore_watch: ['node_modules', 'src/uploads']
+    }
+  ]
+}
